6a7152251f06854fac1f1b3dd935a4aef7b4ffb1
DevHades <70721447+DevHades@users.noreply.github.com>
2020-12-22 23:51:27 +0000

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Auto Announcement Script - Fivem'
+
+author 'https://github.com/DevHades'
+
+version '1.0'
+
+server_scripts {
+  'server.lua'
+}
